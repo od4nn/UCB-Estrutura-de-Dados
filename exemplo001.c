be4ecb7c541ad38bec57d38f43caaf00a4432c8a
Daniel Gama <danielgmbr30@gmail.com>
2026-02-23 12:04:02 -0300

    criando teste

---
+++ exemplo001.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    printf("Hello UCB!");
+    /*system ("rmdir /s Estrutura_de_dados");
+    system("rmdir /s Logica_de_programacao");*/
+    system("dir");
+
+    return 0;
+}

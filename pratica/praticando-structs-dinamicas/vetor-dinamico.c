e30825a25ba66e240ef4ad0f9fecc2802fa3986e
Daniel Gama <danielgmbr30@gmail.com>
2026-02-25 20:07:21 -0300

    usando calloc e malloc

--- pratica/praticando-structs-dinamicas/vetor-dinamico.c
+++ pratica/praticando-structs-dinamicas/vetor-dinamico.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
+    int tam;
+
+    printf("Informe quantos numeros deseja informar: ");
+    scanf("%d", &tam);
+
+    int *vetor = (int*) malloc(sizeof(int) * tam);
+
+    if (vetor == NULL) {
+        return 1;
+    }
+
+    for (int i = 0; i <= tam; i++) {
+        *(vetor+1) = 1 + i;
+        printf("\n%d", vetor[i]);
+    }
+    printf("\nUltimo: %d", *(vetor+1));
+    free(vetor);
+
     return 0;
 }

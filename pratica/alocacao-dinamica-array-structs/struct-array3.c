b18705332fc2ff8bf26f2a3f3bc6288417f8fc0b
Daniel Gama <danielgmbr30@gmail.com>
2026-03-10 09:19:43 -0300

    terminando exercicios de f aprendizado

--- pratica/alocacao-dinamica-array-structs/struct-array3.c
+++ pratica/alocacao-dinamica-array-structs/struct-array3.c
@@ -19,7 +19,10 @@ int main() {
     Pato->p1 = 23;
 
     array[0].p1 = 23;
-    array[1]->p2 = 2;
+    (*(array + 1)).p1 = 12;
+    //array[1]->p2 = 2; errado!
+
+    array = realloc(array, sizeof(Point) * length);
 
     return 0;
 }

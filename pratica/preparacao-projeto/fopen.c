59a160f541b51c6f5ce8509f18f126c8cc154fa0
Daniel Gama <danielgmbr30@gmail.com>
2026-04-04 17:44:57 -0300

    conseguindo separar 2 colunas

--- pratica/preparacao-projeto/fopen.c
+++ pratica/preparacao-projeto/fopen.c
@@ -2,17 +2,28 @@
 #include <string.h>
 
 int main() {
-    FILE *f = fopen("meleia.csv", "r");
+    FILE *f = fopen("teste_TRE-TO.csv", "r");
     if (f == NULL) return 1;
     char LINHA[1000];
+    fgets(LINHA, 1000, f);
+
     while ((fgets(LINHA, 1000, f)) != NULL) {
-
+        char nome1[100], nome2[100];
         char *token = strtok(LINHA, ",");
-
+        int i = 0;
         while (token != NULL) {
-            printf("%s\n", token);
+            if (i == 0) {
+                strcpy(nome1, token);
+            }
+            if (i == 5) {
+                strcpy(nome2, token);
+            }
             token = strtok(NULL, ",");
+
+            i++;
         }
+
+        printf("%s | %s\n", nome1, nome2);
     }
 
     fclose(f);
